dd83a3d09efe4c80632eb7480666b4d5a3da8c79
nestorvave <84485292+nestorvave@users.noreply.github.com>
2022-02-22 18:47:34 -0600

    perimeter added

--- app.js
+++ app.js
@@ -1,40 +1,7 @@
-console.log(5+5)
+function getPerimeter ( h, l ){
+    let perimeter = ((2*(h))+(2*(l)))
 
-function getSmaller(a,b) {
-    return Math.min(a,b)
-}
-console.log(getSmaller(7,1));
-
-function getGreater(a,b) {
-    return Math.max(a,b)
+    return console.log(perimeter)
 }
 
-console.log("msg")
-console.log("2nd message")
-
-const secondPower=(n)=>{
-    return Math.pow(n,2)
-}
-console.log(secondPower(5));
-
-/////////////
-function sayHello (user){
-    return console.log( `hello ${user}`)
-}
-
-sayHello("Nestor")
-
-
-/////////////
-
-function round (value1){
-    return console.log( Math.round(value1))
-}
-
-round (5.51)
-
-const getSquare = (n)=>{
-    return console.log(`Result ${Math.sqrt(n)}`)
-}
-
-getSquare(9)
+getPerimeter(3,5)
