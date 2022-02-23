06219df61ab4fed0270762801df5b3e2a4cb5091
nestorvave <84485292+nestorvave@users.noreply.github.com>
2022-02-22 19:20:54 -0600

    testing

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
-function getPerimeter ( w, l ){
-    let perimeter = ((2*(w))+(2*(l)))
 
-    return console.log(perimeter)
+console.log(5+5)
+
+function getSmaller(a,b) {
+    return Math.min(a,b)
+}
+console.log(getSmaller(7,1));
+
+function getGreater(a,b) {
+    return Math.max(a,b)
 }
 
-getPerimeter(3,5)
+console.log("msg")
+console.log("2nd message")
+
+const secondPower=(n)=>{
+    return Math.pow(n,2)
+}
+console.log(secondPower(5));
+console.log("main")
